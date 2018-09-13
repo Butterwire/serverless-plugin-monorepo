9a272d0cdc578892c884fbe07712e3750a8a173f
Martin Campbell <martin@campbellsoftware.co.uk>
2018-09-13 16:00:36 +0100

    Initial version of plugin.

---
+++ README.md
@@ -0,0 +1,56 @@
+# serverless-plugin-monorepo
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+A Serverless plugin design to make it possible to use Serverless in a 
+Javascript mono repo with hoisted dependencies, e.g. when using [Yarn Workspaces](https://yarnpkg.com/lang/en/docs/workspaces/).
+
+This plugin alleviates the need to use [nohoist](https://yarnpkg.com/blog/2018/02/15/nohoist/) functionality by creating 
+symlinks to all declared dependencies. Development dependencies are deliberately NOT linked so these
+will not be packaged into the resulting archive. 
+
+[Butterwire](https://www.butterwire.com) uses Yarn workspaces and we created this plugin to improve our development
+experience. Not using nohoist saves wasting disk space and also accidentally including
+development dependencies in our packaged functions.
+
+*Note, this package will only work on operating systems that support symbolic links!*
+
+
+## Installation
+
+```
+yarn add --dev serverless-plugin-monorepo
+# or using NPM
+npm install --dev serverless-plugin-monorepo
+```
+
+Currently this plugin requires Node V8+. If there is interest in support older 
+versions then trans-compilation with Babel could be added.
+
+
+## Usage
+
+Add the plugin to your `serverless.yml` file:
+
+```
+plugins:
+  - serverless-plugin-monorepo
+```
+
+The plugin listens for package lifecycle events. Prior to Serverless packaging
+up the service, it will scan the `package.json` file for dependencies and
+ensure that all dependencies (including transitive dependencies) are symlinked in `node_modules`.
+
+Hence when Serverless creates the archive, it will follow the symlinks and all 
+dependencies will be added as expected. Development/peer dependencies are ignored.
+
+
+## Contributing
+
+We welcome issue reports and pull requests!
+
+There is a small `run` script which will launch Node V8 in a Docker container which
+you may find useful for development purposes.
+
+Note we are using [StandardJS](https://standardjs.com/) and you can run
+the lint tool via `yarn lint` which will attempt to automatically issues like spacing etc. 
---
+++ index.js
@@ -0,0 +1,115 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+// Takes a path and returns all node_modules resolution paths (but not global include paths).
+const getNodeModulePaths = p => {
+  const result = []
+  let paths = p.split(path.sep)
+  while (paths.length) {
+    result.push(path.join(paths.join(path.sep) || path.sep, 'node_modules'))
+    paths.pop()
+  }
+  return result
+}
+
+// Creates a symlink. Ignores if fails to create due to already existing.
+async function link (target, f) {
+  await fs.ensureDir(path.dirname(f))
+  await fs.symlink(target, f)
+    .catch(e => {
+      if (e.code === 'EEXIST') {
+        return
+      }
+      throw e
+    })
+}
+
+class ServerlessMonoRepo {
+  constructor (serverless) {
+    this.serverless = serverless
+    this.hooks = {
+      'package:cleanup': this.clean.bind(this),
+      'package:initialize': this.packageInitialise.bind(this)
+    }
+    this.log = msg => serverless.cli.log(msg)
+
+    // Settings
+    this.settings = this.serverless.service.custom.serverlessMonoRepo || {}
+    this.settings.path = this.settings.path || this.serverless.config.servicePath
+  }
+
+  async linkPackage (name, fromPath, toPath, created) {
+    // Do nothing if already created
+    if (created.has(name)) {
+      return
+    }
+
+    // Obtain list of module resolution paths to use for resolving modules
+    const paths = getNodeModulePaths(fromPath)
+
+    // Get package file path
+    const pkg = require.resolve(path.join(name, 'package.json'), { paths })
+
+    // Get relative path to package & create link if not an embedded node_modules
+    const target = path.relative(path.join(toPath, path.dirname(name)), path.dirname(pkg))
+    if ((pkg.match(/node_modules/g) || []).length <= 1) {
+      created.add(name)
+      await link(target, path.join(toPath, name))
+    }
+
+    // Get dependencies
+    const { dependencies = {} } = require(pkg)
+
+    // Link all dependencies
+    await Promise.all(Object.keys(dependencies).map(name =>
+      this.linkPackage(name, path.dirname(pkg), toPath, created)
+    ))
+  }
+
+  async clean () {
+    // Remove all symlinks that are of form [...]/node_modules/link
+    this.log('Cleaning dependency symlinks')
+
+    // Checks if a given stat result indicates a scoped package directory
+    const isScopedPkgDir = c => c.s.isDirectory() && c.f.startsWith('@')
+
+    // Cleans all links in a specific path
+    async function clean (p) {
+      const files = await fs.readdir(p)
+      let contents = await Promise.all(files.map(f =>
+        fs.lstat(path.join(p, f)).then(s => ({ f, s }))
+      ))
+
+      // Remove all links
+      await Promise.all(contents.filter(c => c.s.isSymbolicLink())
+        .map(c => fs.unlink(path.join(p, c.f))))
+      contents = contents.filter(c => !c.s.isSymbolicLink())
+
+      // Remove all links in scoped packages
+      await Promise.all(contents.filter(isScopedPkgDir)
+        .map(c => clean(path.join(p, c.f))))
+      contents = contents.filter(c => !isScopedPkgDir(c))
+
+      // Remove directory if empty
+      if (!contents.length) {
+        await fs.rmdir(p)
+      }
+    }
+
+    // Clean node_modules
+    await clean(path.join(this.settings.path, 'node_modules'))
+  }
+
+  async packageInitialise () {
+    // Read package JSON
+    const { dependencies = {} } = require(path.join(this.settings.path, 'package.json'))
+
+    // Link all dependent packages
+    this.log('Creating dependency symlinks')
+    await Promise.all(Object.keys(dependencies).map(name =>
+      this.linkPackage(name, this.settings.path, path.join(this.settings.path, 'node_modules'), new Set())
+    ))
+  }
+}
+
+module.exports = ServerlessMonoRepo
